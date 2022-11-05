69bb4a99416f6b4a6bf7641a9ba881610d3ea1ec
Abduhamid <Abdukhamid000@gmail.com>
2022-11-05 17:20:03 +0500

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import classes from "../../styles/Navbar.module.css";
 import personal from "../assets/personal.svg";
 import Image from "next/image";
+import notify from "../assets/notification.svg";
 
 const Navbar = () => {
   return (
     <header className={classes.header}>
       <nav className="container d-flex">
         <h3>Logo</h3>
+        <Image src={notify} alt="" />
         <button className={classes.headbtn}>
           Личный кабинет
           <span>
--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,9 +1,62 @@
 import React from "react";
+import classes from "../../styles/Stock.module.css";
+import sale from "../assets/sale.svg";
+import bundle from "../assets/bundle.svg";
+import Image from "next/image";
 
 const Stock = () => {
   return (
-    <div>
+    <div className={classes.stockCon}>
       <h5>Акции</h5>
+      <div className={classes.col}>
+        <div className={classes.top}>
+          <span className="badge">
+            <Image src={sale} alt="sale" />
+          </span>
+          <p>Обменяй свой старый айфон и получи скидку на новый</p>
+          <label>
+            <input type="checkbox" />
+          </label>
+        </div>
+        <small>- 400 000 сум</small>
+      </div>
+      <div className={classes.col}>
+        <div className={classes.top}>
+          <span className="bundle">
+            <Image src={bundle} alt="bundle" />
+          </span>
+          <p>Обменяй свой старый айфон и получи скидку на новый</p>
+          <label>
+            <input type="checkbox" />
+          </label>
+        </div>
+        <small>- 400 000 сум</small>
+      </div>
+      <div className={classes.col}>
+        <div className={classes.top}>
+          <span className="badge">
+            <Image src={sale} alt="sale" />
+          </span>
+          <p>Обменяй свой старый айфон и получи скидку на новый</p>
+          <label>
+            <input type="checkbox" checked />
+          </label>
+        </div>
+        <small>- 400 000 сум</small>
+      </div>
+      <div className={classes.col}>
+        <div className={classes.top}>
+          <span className="bundle">
+            <Image src={bundle} alt="bundle" />
+          </span>
+          <p>Обменяй свой старый айфон и получи скидку на новый</p>
+          <label>
+            <input type="checkbox" checked />
+          </label>
+        </div>
+        <small>- 400 000 сум</small>
+      </div>
+      <button className={classes.btn}>Добавить в корзину</button>
     </div>
   );
 };
